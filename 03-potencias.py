c4d9be78c64ae0ecc66db90c2661dd3f55819311
Santiago <machiiago19@gmail.com>
2021-05-17 19:11:31 -0500

    Corregidos algunos errores en potencia. NO resuelto

--- 03-potencias.py
+++ 03-potencias.py
@@ -1,14 +1,16 @@
 def potencias (base, limite):
     i = 0
+    a = 0
     while a < limite:
-        a = base **i
+        a = base ** i
         print (a)
+        i += 1
 
 
 def run():
     print("Potencias!")
     base = int(input("Inserte el número del que quiere hallar potencias:"))
-    limite = int(input("Inserte el límite máximo"))
+    limite = int(input("Inserte el límite máximo: "))
     potencias(base, limite)
 
 if __name__ == "__main__":
